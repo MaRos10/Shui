dd93ddd74ccc10a9a5f372c316c35f2d54e814fe
’Marina <148355147+MaRos10@users.noreply.github.com>
2024-09-26 08:50:50 +0200

    Check if msgId exists

--- shui-backend/functions/updateMessage/index.js
+++ shui-backend/functions/updateMessage/index.js
@@ -5,55 +5,56 @@ exports.handler = async (event) => {
   try {
     const { id } = event.pathParameters;
 
-    // Kontrollera att id finns med i requesten
     if (!id) {
-      return sendError(400, "Meddelande ID krävs.");
+      return sendError(400, "Meddelande ID krävs");
     }
 
     const body = JSON.parse(event.body);
     const { text } = body;
 
-    // Validering av inkommande data
     if (!text) {
       return sendError(400, "Text krävs för att uppdatera meddelandet");
     }
 
-    // Sätta upp DynamoDB-parametrar för att uppdatera meddelandet
-    const params = {
+    // Kontrollera om meddelande med angivet ID existerar
+    const getParams = {
       TableName: "messages",
       Key: {
-        id: id, // Nyckeln för det meddelande som ska uppdateras
+        id: id,
+      },
+    };
+
+    // Hämta befintligt meddelande
+    const existingMessage = await db.get(getParams);
+
+    // Om meddelandet inte existerar
+    if (!existingMessage.Item) {
+      return sendError(404, "Meddelandet hittades inte");
+    }
+
+    // DB-parametrar för att uppdatera meddelandet
+    const updateParams = {
+      TableName: "messages",
+      Key: {
+        id: id,
       },
       UpdateExpression: "set #text = :text",
       ExpressionAttributeNames: {
         "#text": "text",
       },
       ExpressionAttributeValues: {
-        ":text": text, // Den nya texten som ska sättas
+        ":text": text,
       },
-      ConditionExpression: "attribute_exists(id)", // Kontrollera om meddelandet finns
+      ConditionExpression: "attribute_exists(id)",
       ReturnValues: "UPDATED_NEW", // Returnera de nya värdena efter uppdatering
     };
 
     // Försök att uppdatera meddelandet
-    const result = await db.update(params);
+    const result = await db.update(updateParams);
 
-    // Om inget fält uppdaterades, returnera ett fel
-    if (!result.Attributes) {
-      return sendError(404, "Meddelandet hittades inte");
-    }
-
-    // Returnera det uppdaterade meddelandet
+    // Returnera svar beroende på utfall
     return sendResponse(result.Attributes);
   } catch (error) {
-    console.error("Fel vid uppdatering av meddelande:", error);
-
-    // Om meddelandet inte finns
-    if (error.code === "ConditionalCheckFailedException") {
-      return sendError(404, "Meddelandet hittades inte");
-    }
-
-    // Om ett annat fel uppstod
     return sendError(500, "Ett fel uppstod vid uppdatering av meddelandet");
   }
 };
